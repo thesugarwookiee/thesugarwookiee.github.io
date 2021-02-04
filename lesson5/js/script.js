4e18884e0e6d38737e3dfc44087072db04bee10f
thesugarwookiee <mutedsky@gmail.com>
2021-02-04 09:24:45 -1000

    ddddddddddd

---
+++ lesson5/js/script.js
@@ -0,0 +1,16 @@
+var current = new Date();
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+document.getElementById("date").innerHTML = day[current.getDay()] +
+    ", " + current.getDate() + " " + month[current.getMonth()] + " " +
+    current.getFullYear();
+
+
+
+const toggle = document.querySelector('.toggle');
+const nav = document.querySelector('.navigation')
+    
+toggle.addEventListener('click', () => {nav.classList.toggle('responsive')}, false);
+    
+window.onresize = () => {if (window.innerWidth > 650) nav.classList.remove('responsive')};
