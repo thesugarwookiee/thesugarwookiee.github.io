c00a04695dcad9017fd70323c7f06f80fc008915
thesugarwookiee <mutedsky@gmail.com>
2021-02-17 16:42:41 -1000

    sdfsd

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,30 +1,30 @@
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 const loadImages = (image) => {
-    image.setAttribute('src', image.getAttribute('data-src'));
-    image.onload = () => {
-        image.removeAttribute('data-src');
-    };
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
 };
 
-imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+const imageOptions = {
+    threshold: 0,
+    rootMargin: '0px 0px 50px 0px'
+};
 
-
-if ('IntersectionObserver' in window) {
+if('IntersectionObserver' in window) {
     const observer = new IntersectionObserver((items, observer) => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+      items.forEach((item) => {
+        if(item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
     imagesToLoad.forEach((img) => {
-        observer.observe(img);
+      observer.observe(img);
     });
-} else {
+  } else {
     imagesToLoad.forEach((img) => {
-        loadImages(img);
+      loadImages(img);
     });
-}
+  }
