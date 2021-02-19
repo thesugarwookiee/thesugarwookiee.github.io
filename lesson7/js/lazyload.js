87695d0acf5673c78a5c6163f456d6daed4f5277
thesugarwookiee <mutedsky@gmail.com>
2021-02-19 10:54:07 -1000

    sssss

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -7,24 +7,26 @@ const loadImages = (image) => {
 };
 
 const imageOptions = {
-    threshold: 0,
-    rootMargin: '0px 0px 50px 0px'
+  threshold: 0,
+  rootMargin: '0px 0px 50px 0px'
 };
 
-if('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer) => {
-      items.forEach((item) => {
-        if(item.isIntersecting) {
-          loadImages(item.target);
-          observer.unobserve(item.target);
-        }
-      });
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-      observer.observe(img);
-    });
-  } else {
-    imagesToLoad.forEach((img) => {
-      loadImages(img);
-    });
-  }
+  });
+
+
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
