e846fb13dab4dbab3de29fed7fa4a19051547390
thesugarwookiee <mutedsky@gmail.com>
2021-03-01 09:56:25 -1000

    qdasda

--- lesson8/js/script.js
+++ lesson8/js/script.js
@@ -26,20 +26,6 @@ if (current.getDay() == 5) {
     document.getElementById("event").style.display = "block";
 }
 
-
-//show wind chill if applicable in weather summary
-var tmp = parseInt(document.getElementById("temp").innerText);
-var spd = parseInt(document.getElementById("speed").innerText);
-
-var chill = 35.74 + 0.6215 * tmp - 35.75 * Math.pow(spd, 0.16) + 0.4275 * tmp * Math.pow(spd, 0.16);
-chill = chill.toFixed(0);
-
-if ((tmp > 50) || (spd < 3)) {
-    document.getElementById("wind").innerHTML = "None";
-} else {
-    document.getElementById("wind").innerHTML = chill + "&deg;F";
-}
-
 //show the level of the storm range severity
 function adjustNumber(stormseverity) {
     document.getElementById("stormvalue").innerHTML = stormseverity;
