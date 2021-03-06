d15ca5c884ed655353cec5f62e7c3dcb97a04a57
thesugarwookiee <mutedsky@gmail.com>
2021-03-06 10:23:27 -1000

    fweeop'

---
+++ lesson9/js/city.js
@@ -0,0 +1,52 @@
+const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+
+  .then(function (jsonObject) {
+    const cities = jsonObject['towns'];
+    function filterByName(item) {
+      if (item.name == "Preston" || item.name == "Soda Springs" || item.name == "Fish Haven") {
+        return true;
+      }
+    };
+
+    let singleCity = cities.filter(filterByName);
+
+    for (let i = 0; i < singleCity.length; i++) {
+      let place = document.createElement('section');
+      let cityInfo = document.createElement('dl');
+      cityInfo.setAttribute('class', 'cityInfo');
+      let photo = document.createElement('img');
+      let name = document.createElement('h3');
+      let motto = document.createElement('h4');
+      let yearFounded = document.createElement('div');
+      let foundedNum = document.createElement('p');
+      let currentPop = document.createElement('div');
+      let popNum = document.createElement('p');
+      let averageRain = document.createElement('div');
+      let rainNum = document.createElement('p');
+
+      name.textContent = singleCity[i].name;
+      motto.textContent = singleCity[i].motto;
+      foundedNum.textContent = "Year Founded: " + singleCity[i].yearFounded;
+      popNum.textContent = "Population: " + singleCity[i].currentPopulation;
+      rainNum.textContent = "Average rainfall: " + singleCity[i].averageRainfall + "\"";
+      photo.setAttribute('src', "images/" + singleCity[i].photo);
+      photo.setAttribute('alt', "A scenic image of " + singleCity[i].name);
+
+      place.appendChild(photo);
+      place.appendChild(name);
+      place.appendChild(motto);
+      place.appendChild(cityInfo);
+      cityInfo.appendChild(yearFounded);
+      yearFounded.appendChild(foundedNum);
+      cityInfo.appendChild(currentPop);
+      currentPop.appendChild(popNum);
+      cityInfo.appendChild(averageRain);
+      averageRain.appendChild(rainNum);
+      document.querySelector('div.cities').appendChild(place);
+    }
+  });
