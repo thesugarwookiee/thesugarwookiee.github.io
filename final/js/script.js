5719430765dd88e9621c51c5ae921e5ea5e4f8a3
thesugarwookiee <mutedsky@gmail.com>
2021-04-01 12:32:55 -1000

    wwwwwwwwww

--- final/js/script.js
+++ final/js/script.js
@@ -13,11 +13,11 @@ const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.navigation')
 
 toggle.addEventListener('click', () => {
-    nav.classList.toggle('responsive')
+    nav.classList.toggle('toggle')
 }, false);
 
 window.onresize = () => {
-    if (window.innerWidth > 650) nav.classList.remove('responsive')
+    if (window.innerWidth > 650) nav.classList.remove('toggle')
 };
 
 //show the level of the storm range severity
