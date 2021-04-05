d181404df321963465290272153fdb8416d80cf3
thesugarwookiee <mutedsky@gmail.com>
2021-04-05 11:00:51 -1000

    reeeeee

--- final/js/script.js
+++ final/js/script.js
@@ -1,13 +1,3 @@
-//get date for footer
-var current = new Date();
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-
-document.getElementById("date").innerHTML = day[current.getDay()] +
-    ", " + current.getDate() + " " + month[current.getMonth()] + " " +
-    current.getFullYear();
-
-
 //make toggle menu for small view
 const toggle = document.querySelector('.toggle');
 const nav = document.querySelector('.navigation')
@@ -20,7 +10,6 @@ window.onresize = () => {
     if (window.innerWidth > 650) nav.classList.remove('toggle')
 };
 
-//show the level of the storm range severity
-function adjustNumber(stormseverity) {
-    document.getElementById("stormvalue").innerHTML = stormseverity;
-}
+//get modified
+var moder = document.lastModified;
+document.getElementById("mod").innerHTML = moder;
--- final/js/weather.js
+++ final/js/weather.js
@@ -1,54 +1,33 @@
-// show wind chill if applicable in weather summary
-// var tmp = parseInt(document.getElementById("temp").innerText);
-// var spd = parseInt(document.getElementById("speed").innerText);
+const apiURL = 'https://api.openweathermap.org/data/2.5/onecall?lat=21.4508&lon=-158.0096&units=imperial&appid=11256484b2b104b0c4bb05797b326d5c';
 
-// var chill = 35.74 + 0.6215 * tmp - 35.75 * Math.pow(spd, 0.16) + 0.4275 * tmp * Math.pow(spd, 0.16);
-// chill = chill.toFixed(0);
-
-// if ((tmp > 50) || (spd < 3)) {
-//     document.getElementById("wind").innerHTML = "None";
-// } else {
-//     document.getElementById("wind").innerHTML = chill + "&deg;F";
-// }
-
-let cityID = 5604473;
-const apiWeather = `https://api.openweathermap.org/data/2.5/weather?id=${cityID}&units=imperial&appid=11256484b2b104b0c4bb05797b326d5c`
-const apiForecast = `https://api.openweathermap.org/data/2.5/forecast?id=${cityID}&units=imperial&appid=11256484b2b104b0c4bb05797b326d5c`
-
-fetch(apiWeather)
+fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        let tmp = parseFloat(jsObject.main.temp);
-        let spd = parseFloat(jsObject.wind.speed);
-        let wind = "None";
-        if (tmp <= 50 && spd >= 3) {
-            let chill = (35.74 + (0.6215 * tmp)) - (35.75 * (Math.pow(spd, 0.16))) + (0.4275 * (tmp * (Math.pow(spd, 0.16))));
-            wind = Math.round(chill) + "°F";
-        };
+        document.getElementById('current').textContent = jsObject.current.weather[0].description;
+        const imagesrc = 'https://openweathermap.org/img/wn/' + jsObject.current.weather[0].icon + '@2x.png';
+        const imgalt = jsObject.current.weather[0].description;
 
-        document.getElementById('current').innerHTML = jsObject.weather[0].description;
-        document.getElementById('temp').innerHTML = Math.round(tmp) + "°F";
-        document.getElementById('wind').innerHTML = wind;
-        document.getElementById('speed').innerHTML = Math.round(spd) + " mph";
-        document.getElementById('humid').innerHTML = jsObject.main.humidity + "%";
+        document.getElementById('temp').textContent = Math.round(jsObject.current.temp) + "°F";
+        document.getElementById('humid').textContent = jsObject.current.humidity + "%";
+        document.getElementById('currenticon').setAttribute('src', imagesrc);
+        document.getElementById('currenticon').setAttribute('alt', imgalt);
     });
 
 
+// fetch(apiForecast)
+//     .then((response) => response.json())
+//     .then((castObject) => {
+//         var forecast = castObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+//         const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-fetch(apiForecast)
-    .then((response) => response.json())
-    .then((castObject) => {
-        var forecast = castObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-        const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+//         for (let day = 0; day < forecast.length; day++) {
+//             const d = new Date(forecast[day].dt_txt);
+//             const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+//             const imgalt = forecast[day].weather[0].description;
 
-        for (let day = 0; day < forecast.length; day++) {
-            const d = new Date(forecast[day].dt_txt);
-            const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-            const imgalt = forecast[day].weather[0].description;
-
-            document.getElementById(`day${day+1}`).textContent = weekday[d.getDay()];
-            document.getElementById(`cast${day+1}`).textContent = Math.round(forecast[day].main.temp) + "°F";
-            document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-            document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
-        }
-    });
+//             document.getElementById(`day${day+1}`).textContent = weekday[d.getDay()];
+//             document.getElementById(`cast${day+1}`).textContent = Math.round(forecast[day].main.temp) + "°F";
+//             document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+//             document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
+//         }
+//     });
