33b7e62feab444d1e18f39cf8e3710d7b8aa137b
thesugarwookiee <mutedsky@gmail.com>
2021-04-05 14:44:35 -1000

    mrew

--- final/js/weather.js
+++ final/js/weather.js
@@ -11,23 +11,26 @@ fetch(apiURL)
         document.getElementById('humid').textContent = jsObject.current.humidity + "%";
         document.getElementById('currenticon').setAttribute('src', imagesrc);
         document.getElementById('currenticon').setAttribute('alt', imgalt);
+
+        var forecast = jsObject.daily.slice(1, 4);
+        const weekdays = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+        for (let day = 0; day < forecast.length; day++) {
+            const d = new Date(forecast[day].dt * 1000);
+            const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
+            const desc = forecast[day].weather[0].description;
+
+            document.getElementById(`day${day+1}`).textContent = weekdays[d.getDay()];
+            document.getElementById(`cast${day+1}`).textContent = Math.round(forecast[day].temp.max) + "°F";
+            document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
+            document.getElementById(`icon${day+1}`).setAttribute('alt', desc);
+        }
+
+        if (jsObject.hasOwnProperty('alerts')) {
+            document.querySelector('.walert').style.display = 'block';
+            document.getElementById('alert').innerHTML = jsObject.alerts[0].event;
+            document.getElementById('alertinfo').innerHTML = jsObject.alerts[0].description;
+        } else {
+            document.querySelector('.walert').style.display = 'none';
+        }
     });
-
-
-// fetch(apiForecast)
-//     .then((response) => response.json())
-//     .then((castObject) => {
-//         var forecast = castObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-//         const weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-//         for (let day = 0; day < forecast.length; day++) {
-//             const d = new Date(forecast[day].dt_txt);
-//             const imagesrc = 'https://openweathermap.org/img/wn/' + forecast[day].weather[0].icon + '@2x.png';
-//             const imgalt = forecast[day].weather[0].description;
-
-//             document.getElementById(`day${day+1}`).textContent = weekday[d.getDay()];
-//             document.getElementById(`cast${day+1}`).textContent = Math.round(forecast[day].main.temp) + "°F";
-//             document.getElementById(`icon${day+1}`).setAttribute('src', imagesrc);
-//             document.getElementById(`icon${day+1}`).setAttribute('alt', imgalt);
-//         }
-//     });
