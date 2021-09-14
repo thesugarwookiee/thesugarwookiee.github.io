24b634c0bff09faec851a5d41a3a0341c5d4ecc1
thesugarwookiee <mutedsky@gmail.com>
2021-09-14 13:38:19 -1000

    weeps

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function () {
+    var ol = document.createElement('ol');
+    ol.setAttribute('id', 'linkList');
+
+    const listLinks = [{
+            label: "Week One",
+            url: 'week1/index.html'
+        },
+        {
+            label: "Week Two",
+            url: 'week2/index.html'
+        }
+    ];
+
+    document.getElementById('createList').appendChild(ol);
+    listLinks.forEach(createassignmentList);
+
+    function createassignmentList(item) {
+        var li = document.createElement('li');
+        li.setAttribute('class', 'item');
+        var a = document.createElement('a');
+
+        ol.appendChild(li);
+        li.appendChild(a);
+        a.setAttribute('href', item.url);
+
+        a.innerHTML = item.label;
+    }
+})();
