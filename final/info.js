87980850b1d2cca8b11e09fc0395480a9031d72b
thesugarwookiee <mutedsky@gmail.com>
2021-12-02 10:05:40 -1000

    quote update

--- final/info.js
+++ final/info.js
@@ -4,16 +4,22 @@
 //   .then(response => response.json())
 //   .then(data => console.log(data));
 
-async function updateQuote() {
-    // fetch a random quote from the Quotable API
-    const response = await fetch("https://api.quotable.io/random");
-    const data = await response.json();
-    if (response.ok) {
-        // update HTML
-        quote.textContent = data.content;
-        cite.textContent = data.author;
-    } else {
-        quote.textContent = "An error occured";
-        console.log(data);
+document.addEventListener("DOMContentLoaded", () => {
+    async function updateQuote() {
+        // fetch a random quote from the Quotable API
+        const response = await fetch("https://api.quotable.io/random");
+        const data = await response.json();
+        if (response.ok) {
+            // update HTML
+            document.getElementById("quote").innerHTML = data.content;
+            document.getElementById("author").innerHTML = "★ " + data.author;
+        } else {
+            document.getElementById("quote").innerHTML = "An error occured";
+            console.log(data);
+        }
     }
-}
+
+
+    // throw quote on page load
+    updateQuote();
+});
