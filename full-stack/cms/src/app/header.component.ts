2aeaa1911e015a021c84e7fa63c2e2ad592d34a9
thesugarwookiee <mutedsky@gmail.com>
2022-09-21 14:30:14 -1000

    contact list

--- full-stack/cms/src/app/header.component.ts
+++ full-stack/cms/src/app/header.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cms-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
