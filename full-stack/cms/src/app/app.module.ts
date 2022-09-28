632cad72fade88697662333295655f7e49b7ecea
thesugarwookiee <mutedsky@gmail.com>
2022-09-28 10:57:39 -1000

    messages

--- full-stack/cms/src/app/app.module.ts
+++ full-stack/cms/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentItemComponent } from './documents/document-item/document-item.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,10 @@ import { DocumentDetailComponent } from './documents/document-detail/document-de
     DocumentsComponent,
     DocumentListComponent,
     DocumentItemComponent,
-    DocumentDetailComponent
+    DocumentDetailComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent
   ],
   imports: [
     BrowserModule
