5a6bd99d0dcff6311f0d9bfc3a22dccbe038148e
thesugarwookiee <mutedsky@gmail.com>
2022-09-28 11:25:46 -1000

    navigation

--- full-stack/cms/src/app/app.component.ts
+++ full-stack/cms/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cms';
+  title: string = 'cms';
+  selectedFeature: string = 'documents';
+
+  switchView(selectedFeature: string) {
+    this.selectedFeature = selectedFeature;
+  }
 }
