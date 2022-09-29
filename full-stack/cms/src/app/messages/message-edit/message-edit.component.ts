908af0f71c61a0d205b051a513a3e774d2ce9a8c
thesugarwookiee <mutedsky@gmail.com>
2022-09-28 14:03:56 -1000

    messages

--- full-stack/cms/src/app/messages/message-edit/message-edit.component.ts
+++ full-stack/cms/src/app/messages/message-edit/message-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, Output, EventEmitter, Input } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./message-edit.component.css']
 })
 export class MessageEditComponent implements OnInit {
+  currentSender: string = 'Sierra';
+  @ViewChild('subject', { static: true }) subjectRef: ElementRef;
+  @ViewChild('msgText', { static: true }) msgTextRef: ElementRef;
+  @Output() addMessageEvent = new EventEmitter<Message>();
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSendMessage() {
+    let currentSubject = this.subjectRef.nativeElement.value;
+    let currentMsg = this.msgTextRef.nativeElement.value;
+    const newMessage = new Message(66, currentSubject, currentMsg, this.currentSender);
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.subjectRef.nativeElement.value = '';
+    this.msgTextRef.nativeElement.value = '';
   }
 
 }
--- full-stack/cms/src/app/messages/message-list/message-list.component.ts
+++ full-stack/cms/src/app/messages/message-list/message-list.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
+
 export class MessageListComponent implements OnInit {
+  messages: Message[] = [
+    new Message(1, 'First', 'I have the high ground.', 'General Kenobi'),
+    new Message(2, 'Second', 'Did you ever hear the tragedy of Darth Plagueis the Wise?', 'Palpatine'),
+    new Message(3, 'Third', 'What if the democracy we thought we were serving no longer exists?”', 'Padme'),
+    new Message(4, 'Fourth', 'Rebellions are built on hope.', 'Andor')
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddMessage(message: Message) {
+    this.messages.push(message);
   }
 
 }
