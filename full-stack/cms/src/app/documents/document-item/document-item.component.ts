6a3162199943b37eac59b59caa96a07905294ce5
thesugarwookiee <mutedsky@gmail.com>
2022-10-03 12:41:55 -1000

    document start

--- full-stack/cms/src/app/documents/document-item/document-item.component.ts
+++ full-stack/cms/src/app/documents/document-item/document-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-item',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./document-item.component.css']
 })
 export class DocumentItemComponent implements OnInit {
+  @Input() document: Document;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
