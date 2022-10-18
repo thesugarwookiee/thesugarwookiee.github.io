b2be893b1038c83a5b4d2b4fd6da277bded7aa84
thesugarwookiee <mutedsky@gmail.com>
2022-10-18 11:29:09 -1000

    recipe routing

--- full-stack/recipe/src/app/header/header.component.ts
+++ full-stack/recipe/src/app/header/header.component.ts
@@ -1,20 +1,14 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output() featureSelected = new EventEmitter<string>();
+export class HeaderComponent {
+  // @Output() featureSelected = new EventEmitter<string>();
 
-  onSelect(feature: string){
-    this.featureSelected.emit(feature);
-  }
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // onSelect(feature: string){
+  //   this.featureSelected.emit(feature);
+  // }
 }
--- full-stack/recipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ full-stack/recipe/src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../../recipe.model';
-import { RecipeService } from '../../recipe.service';
+// import { RecipeService } from '../../recipe.service';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,14 +9,15 @@ import { RecipeService } from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
   @Input() recipe: Recipe;
+  @Input() index: number;
 
-  constructor(private recipeService: RecipeService) { }
+  // constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
   }
 
-  onSelected() {
-    this.recipeService.recipeSelected.emit(this.recipe);
-  }
+  // onSelected() {
+  //   this.recipeService.recipeSelected.emit(this.recipe);
+  // }
 
 }
