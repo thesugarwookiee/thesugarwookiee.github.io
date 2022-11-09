e8700c6a6d429d6cca7db90c3a70e95226f27ab0
thesugarwookiee <mutedsky@gmail.com>
2022-11-09 11:27:57 -1000

    http recipes

--- full-stack/recipe/src/app/recipes/recipe.service.ts
+++ full-stack/recipe/src/app/recipes/recipe.service.ts
@@ -8,29 +8,36 @@ import { Recipe } from "./recipe.model";
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe('School Lunch',
-            'Standard school lunch with all the trimmings.',
-            'https://dailynorthwestern.com/wp-content/uploads/2022/05/summerlunch-Gemma-DeCetra.png',
-            [
-                new Ingredient('premade hamburger', 1),
-                new Ingredient('tater tot', 12),
-                new Ingredient('orange', 1),
-                new Ingredient('milk', 1),
-                new Ingredient('vegetable stick', 5)
-            ]),
-        new Recipe('Baked Avocado',
-            'Careful or you might not be able to buy a house.',
-            'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg',
-            [
-                new Ingredient('avocado', 2),
-                new Ingredient('egg', 4),
-                new Ingredient('bacon', 6),
-                new Ingredient('chives', 2)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('School Lunch',
+    //         'Standard school lunch with all the trimmings.',
+    //         'https://dailynorthwestern.com/wp-content/uploads/2022/05/summerlunch-Gemma-DeCetra.png',
+    //         [
+    //             new Ingredient('premade hamburger', 1),
+    //             new Ingredient('tater tot', 12),
+    //             new Ingredient('orange', 1),
+    //             new Ingredient('milk', 1),
+    //             new Ingredient('vegetable stick', 5)
+    //         ]),
+    //     new Recipe('Baked Avocado',
+    //         'Careful or you might not be able to buy a house.',
+    //         'https://hips.hearstapps.com/hmg-prod/images/delish-200114-baked-avocado-boats-0361-landscape-pflo-jpg-1647890967.jpg',
+    //         [
+    //             new Ingredient('avocado', 2),
+    //             new Ingredient('egg', 4),
+    //             new Ingredient('bacon', 6),
+    //             new Ingredient('chives', 2)
+    //         ])
+    // ];
+
+    private recipes: Recipe[] = [];
 
     constructor(private shoppingListService: ShoppingListService) {
+    }
+
+    setRecipes(recipes: Recipe[]) {
+        this.recipes = recipes;
+        this.recipesChanged.next(this.recipes.slice());
     }
 
     getRecipes() {
