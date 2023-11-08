4ddbcfdeadbeeee1821af37d094828cf1fd82743
thesugarwookiee <mutedsky@gmail.com>
2023-11-08 12:38:23 -1000

    done for today

--- photography/js/script.js
+++ photography/js/script.js
@@ -10,29 +10,3 @@ window.onresize = () => {
     if (window.innerWidth > 650) nav.classList.remove('toggle')
 };
 
-//collapse FAQ
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("activelist");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
-
-//random picture for home page
-window.onload = choosePic;
-
-var myPix = new Array("images/1.jpg", "images/2.jpg", "images/3.jpg", "images/4.jpg", "images/5.jpg", "images/6.jpg", "images/7.jpg", "images/8.jpg", "images/9.jpg", "images/10.jpg");
-
-function choosePic() {
-    var randomNum = Math.floor(Math.random() * myPix.length);
-    document.getElementById("myPicture").src = myPix[randomNum];
-}
